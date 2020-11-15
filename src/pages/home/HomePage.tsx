6b9c3b2d61fbb75221c8e2466bc3ccfd1b6fe976
zuratama <tuandt66742@gmail.com>
2020-11-15 22:29:09 +0700

    [Base] Demonstrate private component.

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import HomeBox from "./components/HomeBox";
+
+import "./styles.scss";
 
 function HomePage() {
-  return <div className="text-3xl">Home</div>;
+  return (
+    <div className="flex text-3xl items-center">
+      <span>Home</span>
+      <div className="pl-2">
+        <HomeBox></HomeBox>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
---
+++ src/pages/home/components/HomeBox.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function HomeBox() {
+  return <div className="home-temp-box"></div>;
+}
